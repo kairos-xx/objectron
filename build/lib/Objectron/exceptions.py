246b53682a1fccdeabafe652bdd5392bc8f656e0
kairos <joaoslopes@gmail.com>
2025-01-08 07:56:30 +0000

    Delete build directory

--- build/lib/Objectron/exceptions.py
+++
@@ -1,7 +0,0 @@
-class TransformationError(Exception):
-    """
-    Exception raised when an error occurs during object transformation.
-    """
-
-    def __init__(self, message: str):
-        super().__init__(message)
